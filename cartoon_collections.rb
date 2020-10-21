8903a0b2607e3bad53fc65ba01e98056292dc77a
Emma Fewer <emmafewer20@gmail.com>
2020-10-21 20:48:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,8 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.map do |string|
-    if string == valid_calls[0]
+  planeteer_calls.find do |string|
+    if string == 
       string
     else
       valid_calls+=1
